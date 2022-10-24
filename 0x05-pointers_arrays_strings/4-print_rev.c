5a0ba0dc4d07bd8cd97a8b86cfb1314e8110a512
Olvwatoyosi <oluwatoyosianiyi@gmail.com>
2022-10-24 09:33:47 -0700

    Correction 1

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,20 @@
  */
 void print_rev(char *s)
 {
-	int a = 0;
+	int longi = 0;
+	int o;
 
-	while (s[a] != '\0')
-		a++
-	for (a = a - 1; a >= 0; a--)
-		_putchar(s[a]);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
 
 	_putchar('\n');
 }
