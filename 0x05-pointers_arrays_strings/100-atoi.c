2f10121a50cc5f2c1453827731f335b90982861d
Olvwatoyosi <oluwatoyosianiyi@gmail.com>
2022-10-24 15:17:03 -0700

    correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,36 +7,18 @@
  */
 int _atoi(char *s)
 {
-	i = 0;
-	d = 0;
-	n = 0;
-	length = 0;
-	f = 0;
-	digit = 0;
+	int sign = 1;
+	unsigned int num = 0;
 
-	while (s[length] != '\0')
-		length++;
-	while (i < length && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-	if (f == 0)
-		return (0);
-
-	return (n);
+	return (num * sign);
 }
